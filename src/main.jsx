00f2b904c8783f751c0fc5b9c2f073a8c1469b97
njlazzhra <najlatanjung08@gmail.com>
2025-12-14 20:33:14 +0700

    fix: react router import, add BrowserRouter from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import RedirectLogin from "./components/RedirectLogin.jsx";
 import UserRegister from "./components/User/UserRegister.jsx";
 import UserLogin from "./components/User/UserLogin.jsx";
